a35d269d2461de6dc4f54a09e23ee78dfe9fa501
Jumiati Riyana <jumiatiriyanaa@gmail.com>
2024-09-06 09:50:34 +0700

    Add Val.kt

---
+++ src/main/kotlin/Val.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var varVariabel : Int
+    varVariabel = 56
+    println("Pemberian nilai pertama pada varVariabel = " +varVariabel)
+    varVariabel = 78
+    println("Pemberian nilai kedua pada varVariabel = " +varVariabel)
+    val phi = 3.14
+    //phi = 2.5
+
+}
